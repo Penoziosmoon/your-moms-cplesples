926911016c906f920ac31e40db3a13d788dca959
Penoziosmoon <141031492+Penoziosmoon@users.noreply.github.com>
2023-09-28 17:09:45 +0800

    Update cleandadishes.cpp

--- cleandadishes.cpp
+++ cleandadishes.cpp
@@ -1,5 +1,9 @@
 #include iostream 
 
 int main(void) {
-  std::cout << "SUKA BYLAD NAXUY";
+  int e = 0;
+  std::cout << "You have 10 seconds";
+  while (e < 11) {
+    std::cout << e << "\n";
+    e++;
 }
